f0047544a1baf1fb95f0f768c24e069ff8e747c1
Jordi Nebot <llengot@gmail.com>
2012-09-27 11:22:15 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,60 @@
+var max_timestamp = 1420005600;
+
+var a = new Array();
+
+for (var i=0; i<10; i++) {
+	a[i] = new Array(get_random_value(10), get_random_color(), get_random_date());
+} 
+
+// Funció d'ordenació
+Array.prototype.mysort = function() {
+	this.sort(function(a,b){
+		if (a[2] > b[2]) return  1;
+	    if (a[2] < b[2]) return -1;
+	    return 0;
+	});
+	return this;
+}
+
+$(document).ready(function(){
+	$('#output').append('<h1>Before sort</h1><ol>');
+	for (var i=0; i < a.length; i++) {
+		$('#output').append('<li>' + a[i][2] + '</li>');
+	};
+	$('#output').append('</ol>');
+	$('#output').append('<h1>After sort</h1><ol>');
+
+	b = a.mysort();
+	
+	for (var i=0; i < b.length; i++) {
+		$('#output').append('<li>' + b[i][2] + '</li>');
+	};
+	$('#output').append('</ol>');
+});
+
+
+
+
+
+
+
+
+
+// Funcions genèriques
+
+function get_random_value(upper_limit) {
+	return Math.floor((Math.random() * upper_limit) + 1);
+}
+
+function get_random_color() {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.round(Math.random() * 15)];
+    }
+    return color;
+}
+
+function get_random_date() {
+	return new Date(1900 + get_random_value(200), get_random_value(12), get_random_value(28));
+}
